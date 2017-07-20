918821d5ad9096c92be327505e69276dbf244241
Takashi Ohmori <egao.wo.taisetuni@gmail.com>
2017-07-20 19:00:20 +0000

    Procfile

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,6 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    //Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
 });
