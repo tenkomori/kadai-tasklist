fa25d1ef263a10ef5c60cc04e3528bfdab02721d
Takashi Ohmori <egao.wo.taisetuni@gmail.com>
2017-07-20 19:11:29 +0000

    Procfile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -22,7 +22,7 @@ class TasksController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $tasks = $user->tasks()->orderBy('created_at', 'desc');
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->get();
 
             $data = [
                 'user' => $user,
